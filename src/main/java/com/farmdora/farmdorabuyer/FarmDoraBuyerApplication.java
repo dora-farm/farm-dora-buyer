3c69485e30e391f7d0ad950568bc1ac65499d1f7
leedahoon <ekgns0508@gmail.com>
2025-04-05 00:27:16 +0900

    Project Setting

---
+++ src/main/java/com/farmdora/farmdorabuyer/FarmDoraBuyerApplication.java
@@ -0,0 +1,13 @@
+package com.farmdora.farmdorabuyer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmDoraBuyerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FarmDoraBuyerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/farmdora/farmdorabuyer/entity/Gender.java
@@ -0,0 +1,12 @@
+package com.farmdora.farmdorabuyer.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Gender {
+    MALE(1), FEMALE(2);
+
+    private final int value;
+}
