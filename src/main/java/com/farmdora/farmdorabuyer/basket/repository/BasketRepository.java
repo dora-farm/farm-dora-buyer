cea6ef9e06584e1912a42b0eceb54f1269ef878d
leedahoon <ekgns0508@gmail.com>
2025-04-23 16:19:00 +0900

    feat: repository 추가
    - Basket, Option 레포지토리 인터페이스 추가
    
    FDORA-21

---
+++ src/main/java/com/farmdora/farmdorabuyer/basket/repository/BasketRepository.java
@@ -0,0 +1,8 @@
+package com.farmdora.farmdorabuyer.basket.repository;
+
+import com.farmdora.farmdorabuyer.entity.Basket;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BasketRepository extends JpaRepository<Basket, Integer> {
+
+}
---
+++ src/main/java/com/farmdora/farmdorabuyer/orders/repository/OptionRepository.java
@@ -0,0 +1,7 @@
+package com.farmdora.farmdorabuyer.orders.repository;
+
+import com.farmdora.farmdorabuyer.entity.Option;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OptionRepository extends JpaRepository<Option, Integer> {
+}
