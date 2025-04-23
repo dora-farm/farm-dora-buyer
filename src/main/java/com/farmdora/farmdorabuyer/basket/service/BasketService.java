fc4a4808437923eccf3a6ba9a1cd7ca4daf8df3a
leedahoon <ekgns0508@gmail.com>
2025-04-23 16:19:31 +0900

    feat: 장바구니 추가 서비스 로직 추가
    FDORA-21

---
+++ src/main/java/com/farmdora/farmdorabuyer/basket/service/BasketService.java
@@ -0,0 +1,37 @@
+package com.farmdora.farmdorabuyer.basket.service;
+
+import com.farmdora.farmdorabuyer.basket.dto.BasketRequestDto;
+import com.farmdora.farmdorabuyer.basket.repository.BasketRepository;
+import com.farmdora.farmdorabuyer.common.exception.ResourceNotFoundException;
+import com.farmdora.farmdorabuyer.entity.Basket;
+import com.farmdora.farmdorabuyer.entity.Option;
+import com.farmdora.farmdorabuyer.entity.User;
+import com.farmdora.farmdorabuyer.orders.repository.OptionRepository;
+import com.farmdora.farmdorabuyer.orders.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BasketService {
+    private final BasketRepository basketRepository;
+    private final UserRepository userRepository;
+    private final OptionRepository optionRepository;
+
+    public void addBasket(Integer userId, BasketRequestDto basketAddRequest) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new ResourceNotFoundException("User", userId));
+
+        Option option = optionRepository.findById(basketAddRequest.getOptionId())
+                .orElseThrow(() -> new ResourceNotFoundException("Option", basketAddRequest.getOptionId()));
+
+        Basket basket = Basket.builder()
+                .user(user)
+                .option(option)
+                .quantity(basketAddRequest.getQuantity())
+                .build();
+        basketRepository.save(basket);
+    }
+}
