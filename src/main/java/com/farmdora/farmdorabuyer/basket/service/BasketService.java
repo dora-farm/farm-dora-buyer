053e5be2c217b11af45d20682f33ea438c6d0307
leedahoon <ekgns0508@gmail.com>
2025-04-23 16:41:55 +0900

    feat: 장바구니 이미 존재 여부 검사 로직 추가
    FDORA-21

--- src/main/java/com/farmdora/farmdorabuyer/basket/service/BasketService.java
+++ src/main/java/com/farmdora/farmdorabuyer/basket/service/BasketService.java
@@ -2,12 +2,14 @@ package com.farmdora.farmdorabuyer.basket.service;
 
 import com.farmdora.farmdorabuyer.basket.dto.BasketRequestDto;
 import com.farmdora.farmdorabuyer.basket.repository.BasketRepository;
+import com.farmdora.farmdorabuyer.common.exception.ResourceAlreadyExistsException;
 import com.farmdora.farmdorabuyer.common.exception.ResourceNotFoundException;
 import com.farmdora.farmdorabuyer.entity.Basket;
 import com.farmdora.farmdorabuyer.entity.Option;
 import com.farmdora.farmdorabuyer.entity.User;
 import com.farmdora.farmdorabuyer.orders.repository.OptionRepository;
 import com.farmdora.farmdorabuyer.orders.repository.UserRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -26,6 +28,11 @@ public class BasketService {
 
         Option option = optionRepository.findById(basketAddRequest.getOptionId())
                 .orElseThrow(() -> new ResourceNotFoundException("Option", basketAddRequest.getOptionId()));
+
+        Optional<Basket> existsBasket = basketRepository.findByUserAndOption(user, option);
+        if (existsBasket.isPresent()) {
+            throw new ResourceAlreadyExistsException("Basket", existsBasket.get().getId());
+        }
 
         Basket basket = Basket.builder()
                 .user(user)
