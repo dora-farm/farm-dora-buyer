ec116ffc43731a6a6137dd06da9f3e67785f4833
leedahoon <ekgns0508@gmail.com>
2025-04-23 16:41:36 +0900

    feat: 장바구니 조회 쿼리메서드 추가
    FDORA-21

--- src/main/java/com/farmdora/farmdorabuyer/basket/repository/BasketRepository.java
+++ src/main/java/com/farmdora/farmdorabuyer/basket/repository/BasketRepository.java
@@ -1,8 +1,11 @@
 package com.farmdora.farmdorabuyer.basket.repository;
 
 import com.farmdora.farmdorabuyer.entity.Basket;
+import com.farmdora.farmdorabuyer.entity.Option;
+import com.farmdora.farmdorabuyer.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BasketRepository extends JpaRepository<Basket, Integer> {
-
+    Optional<Basket> findByUserAndOption(User user, Option option);
 }
