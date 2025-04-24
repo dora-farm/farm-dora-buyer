a4b58ad06e5c45e467accb0a6992d698d533dc8b
leedahoon <ekgns0508@gmail.com>
2025-04-24 12:09:20 +0900

    feat: 사용자의 장바구니 개수 조회 쿼리메서드 추가
    FDORA-21

--- src/main/java/com/farmdora/farmdorabuyer/basket/repository/BasketRepository.java
+++ src/main/java/com/farmdora/farmdorabuyer/basket/repository/BasketRepository.java
@@ -8,4 +8,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BasketRepository extends JpaRepository<Basket, Integer> {
     Optional<Basket> findByUserAndOption(User user, Option option);
+    Long countByUser(User user);
 }
