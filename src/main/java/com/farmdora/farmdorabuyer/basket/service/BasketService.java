01d7df26a8f634490e9963c808809bf502f262ea
leedahoon <ekgns0508@gmail.com>
2025-04-24 12:09:50 +0900

    feat: 장바구니 개수 최대치 여부 검사 로직 추가
    FDORA-21

--- src/main/java/com/farmdora/farmdorabuyer/basket/service/BasketService.java
+++ src/main/java/com/farmdora/farmdorabuyer/basket/service/BasketService.java
@@ -1,6 +1,7 @@
 package com.farmdora.farmdorabuyer.basket.service;
 
 import com.farmdora.farmdorabuyer.basket.dto.BasketRequestDto;
+import com.farmdora.farmdorabuyer.basket.exception.BasketOverLimitException;
 import com.farmdora.farmdorabuyer.basket.repository.BasketRepository;
 import com.farmdora.farmdorabuyer.common.exception.ResourceAlreadyExistsException;
 import com.farmdora.farmdorabuyer.common.exception.ResourceNotFoundException;
@@ -11,6 +12,7 @@ import com.farmdora.farmdorabuyer.orders.repository.OptionRepository;
 import com.farmdora.farmdorabuyer.orders.repository.UserRepository;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,6 +24,8 @@ public class BasketService {
     private final UserRepository userRepository;
     private final OptionRepository optionRepository;
 
+    private static final int BASKET_LIMIT = 16;
+
     public void addBasket(Integer userId, BasketRequestDto basketAddRequest) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new ResourceNotFoundException("User", userId));
@@ -29,10 +33,8 @@ public class BasketService {
         Option option = optionRepository.findById(basketAddRequest.getOptionId())
                 .orElseThrow(() -> new ResourceNotFoundException("Option", basketAddRequest.getOptionId()));
 
-        Optional<Basket> existsBasket = basketRepository.findByUserAndOption(user, option);
-        if (existsBasket.isPresent()) {
-            throw new ResourceAlreadyExistsException("Basket", existsBasket.get().getId());
-        }
+        checkBasketAlreadyExists(user, option);
+        checkBasketLimit(user);
 
         Basket basket = Basket.builder()
                 .user(user)
@@ -40,5 +42,19 @@ public class BasketService {
                 .quantity(basketAddRequest.getQuantity())
                 .build();
         basketRepository.save(basket);
+    }
+
+    private void checkBasketAlreadyExists(User user, Option option) {
+        Optional<Basket> existsBasket = basketRepository.findByUserAndOption(user, option);
+        if (existsBasket.isPresent()) {
+            throw new ResourceAlreadyExistsException("Basket", existsBasket.get().getId());
+        }
+    }
+
+    private void checkBasketLimit(User user) {
+        Long basketCount = basketRepository.countByUser(user);
+        if (basketCount >= BASKET_LIMIT) {
+            throw new BasketOverLimitException("장바구니 개수가 최대입니다.", HttpStatus.BAD_REQUEST);
+        }
     }
 }
