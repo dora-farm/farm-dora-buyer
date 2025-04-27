22f0d62d68fab3d0e6c8f91a11f799df6ee4030a
leedahoon <ekgns0508@gmail.com>
2025-04-27 20:54:16 +0900

    refactor: 테스트 코드 일부 리팩토링
    FDORA-38

--- src/test/java/com/farmdora/farmdorabuyer/basket/controller/BasketControllerTest.java
+++ src/test/java/com/farmdora/farmdorabuyer/basket/controller/BasketControllerTest.java
@@ -28,6 +28,7 @@ import com.farmdora.farmdorabuyer.common.exception.ResourceNotFoundException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -42,176 +43,188 @@ class BasketControllerTest {
     private BasketService basketService;
 
     @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
     private MockMvc mvc;
 
-    @Test
+    @Nested
     @DisplayName("장바구니 추가 API 테스트")
-    void testAddBasket() throws Exception {
-        // given
-        doNothing().when(basketService).addBasket(anyInt(), any(BasketRequestDto.class));
+    class AddBasketTests {
 
-        // when
-        // then
-        BasketRequestDto basketAddRequest = BasketRequestDto.builder()
-                .optionId(1)
-                .quantity(10)
-                .build();
-        mvc.perform(post("/api/basket")
-                        .content(new ObjectMapper().writeValueAsString(basketAddRequest))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.status", equalTo(200)))
-                .andExpect(jsonPath("$.message", equalTo(ADD_BASKET_SUCCESS.getMessage())));
+        private BasketRequestDto basketAddRequest = BasketRequestDto.builder()
+                    .optionId(1)
+                    .quantity(10)
+                    .build();
 
-        verify(basketService, times(1)).addBasket(anyInt(), any(BasketRequestDto.class));
+        @Test
+        @DisplayName("장바구니 추가 성공")
+        void testAddBasket() throws Exception {
+            // given
+            doNothing().when(basketService).addBasket(anyInt(), any(BasketRequestDto.class));
+
+            // when
+            // then
+            mvc.perform(post("/api/basket")
+                            .content(objectMapper.writeValueAsString(basketAddRequest))
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.status", equalTo(200)))
+                    .andExpect(jsonPath("$.message", equalTo(ADD_BASKET_SUCCESS.getMessage())));
+
+            verify(basketService, times(1)).addBasket(anyInt(), any(BasketRequestDto.class));
+        }
+
+        @Test
+        @DisplayName("장바구니 추가시 옵션이 존재하지 않을 경우 에러처리 테스트")
+        void testAddBasket_OptionNotFoundException() throws Exception {
+            // given
+            doThrow(new ResourceNotFoundException("Option", 1)).when(basketService).addBasket(anyInt(), any(BasketRequestDto.class));
+
+            // when
+            // then
+            mvc.perform(post("/api/basket")
+                            .content(objectMapper.writeValueAsString(basketAddRequest))
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.status", equalTo(400)))
+                    .andExpect(jsonPath("$.message", equalTo("Option 데이터가 존재하지 않습니다 : '1'")));
+        }
+
+        @Test
+        @DisplayName("장바구니 추가시 담은 수량이 재고수를 넘은 경우 에러처리 테스트")
+        void testAddBasket_QuantityOverLimitException() throws Exception {
+            // given
+            doThrow(new QuantityOverLimitException()).when(basketService).addBasket(anyInt(), any(BasketRequestDto.class));
+
+            // when
+            // then
+            mvc.perform(post("/api/basket")
+                            .content(objectMapper.writeValueAsString(basketAddRequest))
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.status", equalTo(400)))
+                    .andExpect(jsonPath("$.message", equalTo("옵션의 수량보다 많이 담을 수 없습니다.")));
+        }
+
+        @Test
+        @DisplayName("장바구니 추가시 장바구니가 이미 존재할 경우 에러처리 테스트")
+        void testAddBasket_ResourceAlreadyExistsException() throws Exception {
+            // given
+            doThrow(new ResourceAlreadyExistsException("Basket", 1)).when(basketService).addBasket(anyInt(), any(BasketRequestDto.class));
+
+            // when
+            // then
+            mvc.perform(post("/api/basket")
+                            .content(objectMapper.writeValueAsString(basketAddRequest))
+                            .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(status().isConflict())
+                    .andExpect(jsonPath("$.status", equalTo(409)))
+                    .andExpect(jsonPath("$.message", equalTo("Basket 이미 존재하는 데이터입니다. : '1'")));
+        }
     }
 
-    @Test
-    @DisplayName("장바구니 추가시 옵션이 존재하지 않을 경우 에러처리 테스트")
-    void testAddBasket_OptionNotFoundException() throws Exception {
-        // given
-        doThrow(new ResourceNotFoundException("Option", 1)).when(basketService).addBasket(anyInt(), any(BasketRequestDto.class));
-
-        // when
-        // then
-        BasketRequestDto basketAddRequest = BasketRequestDto.builder()
-                .optionId(1)
-                .quantity(10)
-                .build();
-        mvc.perform(post("/api/basket")
-                .content(new ObjectMapper().writeValueAsString(basketAddRequest))
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.status", equalTo(400)))
-                .andExpect(jsonPath("$.message", equalTo("Option 데이터가 존재하지 않습니다 : '1'")));
-    }
-
-    @Test
-    @DisplayName("장바구니 추가시 담은 수량이 재고수를 넘은 경우 에러처리 테스트")
-    void testAddBasket_QuantityOverLimitException() throws Exception {
-        // given
-        doThrow(new QuantityOverLimitException()).when(basketService).addBasket(anyInt(), any(BasketRequestDto.class));
-
-        // when
-        // then
-        BasketRequestDto basketAddRequest = BasketRequestDto.builder()
-                .optionId(1)
-                .quantity(10)
-                .build();
-        mvc.perform(post("/api/basket")
-                        .content(new ObjectMapper().writeValueAsString(basketAddRequest))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.status", equalTo(400)))
-                .andExpect(jsonPath("$.message", equalTo("옵션의 수량보다 많이 담을 수 없습니다.")));
-    }
-
-    @Test
-    @DisplayName("장바구니 추가시 장바구니가 이미 존재할 경우 에러처리 테스트")
-    void testAddBasket_ResourceAlreadyExistsException() throws Exception {
-        // given
-        doThrow(new ResourceAlreadyExistsException("Basket", 1)).when(basketService).addBasket(anyInt(), any(BasketRequestDto.class));
-
-        // when
-        // then
-        BasketRequestDto basketAddRequest = BasketRequestDto.builder()
-                .optionId(1)
-                .quantity(10)
-                .build();
-        mvc.perform(post("/api/basket")
-                        .content(new ObjectMapper().writeValueAsString(basketAddRequest))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isConflict())
-                .andExpect(jsonPath("$.status", equalTo(409)))
-                .andExpect(jsonPath("$.message", equalTo("Basket 이미 존재하는 데이터입니다. : '1'")));
-    }
-
-    @Test
+    @Nested
     @DisplayName("사용자의 장바구니 목록 조회 API 테스트")
-    void testGetBaskets() throws Exception {
-        // given
-        List<BasketResponseDto> baskets = List.of(
-                BasketResponseDto.builder()
-                        .basketId(1)
-                        .title("상품1")
-                        .option("옵션1")
-                        .quantity(1)
-                        .price(1000)
-                        .build(),
-                BasketResponseDto.builder()
-                        .basketId(2)
-                        .title("상품2")
-                        .option("옵션2")
-                        .quantity(2)
-                        .price(2000)
-                        .build()
-        );
-        when(basketService.getBaskets(anyInt())).thenReturn(baskets);
+    class GetBasketsTests {
 
-        // when
-        // then
-        mvc.perform(get("/api/basket"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.status", equalTo(200)))
-                .andExpect(jsonPath("$.message", equalTo(GET_BASKETS_SUCCESS.getMessage())))
-                .andExpect(jsonPath("$.data.size()", equalTo(2)));
+        @Test
+        @DisplayName("사용자의 장바구니 목록 조회 성공")
+        void testGetBaskets() throws Exception {
+            // given
+            List<BasketResponseDto> baskets = List.of(
+                    BasketResponseDto.builder()
+                            .basketId(1)
+                            .title("상품1")
+                            .option("옵션1")
+                            .quantity(1)
+                            .price(1000)
+                            .build(),
+                    BasketResponseDto.builder()
+                            .basketId(2)
+                            .title("상품2")
+                            .option("옵션2")
+                            .quantity(2)
+                            .price(2000)
+                            .build()
+            );
+            when(basketService.getBaskets(anyInt())).thenReturn(baskets);
+
+            // when
+            // then
+            mvc.perform(get("/api/basket"))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.status", equalTo(200)))
+                    .andExpect(jsonPath("$.message", equalTo(GET_BASKETS_SUCCESS.getMessage())))
+                    .andExpect(jsonPath("$.data.size()", equalTo(2)));
+        }
     }
 
-    @Test
+    @Nested
     @DisplayName("사용자의 장바구니 삭제 API 테스트")
-    void testRemoveBasket() throws Exception {
-        // given
-        doNothing().when(basketService).removeBasket(anyInt(), anyInt());
+    class RemoveBasketsTest {
 
-        // when
-        // then
-        mvc.perform(delete("/api/basket/{basketId}", 1))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.status", equalTo(200)))
-                .andExpect(jsonPath("$.message", equalTo(REMOVE_BASKET_SUCCESS.getMessage())));
+        @Test
+        @DisplayName("사용자의 장바구니 삭제 성공")
+        void testRemoveBasket() throws Exception {
+            // given
+            doNothing().when(basketService).removeBasket(anyInt(), anyInt());
+
+            // when
+            // then
+            mvc.perform(delete("/api/basket/{basketId}", 1))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.status", equalTo(200)))
+                    .andExpect(jsonPath("$.message", equalTo(REMOVE_BASKET_SUCCESS.getMessage())));
+        }
+
+        @Test
+        @DisplayName("사용자의 장바구니 삭제시 장바구니가 없을 경우 에러처리 테스트")
+        void testRemoveBasket_ResourceNotFoundException() throws Exception {
+            // given
+            doThrow(new ResourceNotFoundException("Basket", 1)).when(basketService).removeBasket(anyInt(), anyInt());
+
+            // when
+            // then
+            mvc.perform(delete("/api/basket/{basketId}", 1))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.status", equalTo(400)))
+                    .andExpect(jsonPath("$.message", equalTo("Basket 데이터가 존재하지 않습니다 : '1'")));
+        }
     }
 
-    @Test
-    @DisplayName("사용자의 장바구니 삭제시 장바구니가 없을 경우 에러처리 테스트")
-    void testRemoveBasket_ResourceNotFoundException() throws Exception {
-        // given
-        doThrow(new ResourceNotFoundException("Basket", 1)).when(basketService).removeBasket(anyInt(), anyInt());
-
-        // when
-        // then
-        mvc.perform(delete("/api/basket/{basketId}", 1))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.status", equalTo(400)))
-                .andExpect(jsonPath("$.message", equalTo("Basket 데이터가 존재하지 않습니다 : '1'")));
-    }
-
-    @Test
+    @Nested
     @DisplayName("사용자의 장바구니 수량 수정 API 테스트")
-    void testUpdateQuantity() throws Exception{
-        // given
-        doNothing().when(basketService).updateBasketQuantity(anyInt(), anyInt(), anyInt());
+    class UpdateQuantityTests {
 
-        // when
-        // then
-        mvc.perform(put("/api/basket/{basketId}", 1)
-                .param("quantity", "10"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.status", equalTo(200)))
-                .andExpect(jsonPath("$.message", equalTo(UPDATE_BASKET_QUANTITY_SUCCESS.getMessage())));
-    }
+        @Test
+        @DisplayName("사용자의 장바구니 수량 수정 성공")
+        void testUpdateQuantity() throws Exception{
+            // given
+            doNothing().when(basketService).updateBasketQuantity(anyInt(), anyInt(), anyInt());
 
-    @Test
-    @DisplayName("사용자의 장바구니 수량 수정시 장바구니가 존재하지 않을 경우 에러처리 테스트")
-    void testUpdateQuantity_ResourceNotFoundException() throws Exception{
-        // given
-        doThrow(new ResourceNotFoundException("Basket", 1)).when(basketService).updateBasketQuantity(anyInt(), anyInt(), anyInt());
+            // when
+            // then
+            mvc.perform(put("/api/basket/{basketId}", 1)
+                    .param("quantity", "10"))
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.status", equalTo(200)))
+                    .andExpect(jsonPath("$.message", equalTo(UPDATE_BASKET_QUANTITY_SUCCESS.getMessage())));
+        }
 
-        // when
-        // then
-        mvc.perform(put("/api/basket/{basketId}", 1)
-                        .param("quantity", "10"))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.status", equalTo(400)))
-                .andExpect(jsonPath("$.message", equalTo("Basket 데이터가 존재하지 않습니다 : '1'")));
+        @Test
+        @DisplayName("사용자의 장바구니 수량 수정시 장바구니가 존재하지 않을 경우 에러처리 테스트")
+        void testUpdateQuantity_ResourceNotFoundException() throws Exception{
+            // given
+            doThrow(new ResourceNotFoundException("Basket", 1)).when(basketService).updateBasketQuantity(anyInt(), anyInt(), anyInt());
+
+            // when
+            // then
+            mvc.perform(put("/api/basket/{basketId}", 1)
+                            .param("quantity", "10"))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.status", equalTo(400)))
+                    .andExpect(jsonPath("$.message", equalTo("Basket 데이터가 존재하지 않습니다 : '1'")));
+        }
     }
 }
