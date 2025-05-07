af3b26a66c35d8a39566136f4f24844c180ee9d5
leedahoon <ekgns0508@gmail.com>
2025-05-07 15:11:41 +0900

    feat: 쿼리메서드 추가
    FDORA-169

--- src/main/java/com/farmdora/farmdorabuyer/orders/repository/UserRepository.java
+++ src/main/java/com/farmdora/farmdorabuyer/orders/repository/UserRepository.java
@@ -1,7 +1,13 @@
 package com.farmdora.farmdorabuyer.orders.repository;
 
 import com.farmdora.farmdorabuyer.entity.User;
+import io.lettuce.core.dynamic.annotation.Param;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    @Query("select u from User u join fetch u.bankType where u.userId = :userId")
+    Optional<User> findByUserIdWithBankType(@Param("userId") Integer userId);
 }
